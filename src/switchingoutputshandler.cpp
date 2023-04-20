43b2ce2257b235c072b624407d7818b97fe45689
papparazzo <pappi-@gmx.de>
2023-04-20 05:17:40 +0200

    SwitchingOutputsHandler added

---
+++ src/switchingoutputshandler.cpp
@@ -0,0 +1,44 @@
+/*
+ *  Project:    moba-connector
+ * 
+ *  Copyright (C) 2023 Stefan Paproth <pappi-@gmx.de>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ * 
+ */
+
+#include "switchingoutputshandler.h"
+#include "moba/cs2utils.h"
+
+#include <thread>
+#include <utility>
+
+SwitchingOutputsHandler::SwitchingOutputsHandler(EndpointPtr endpoint, CS2WriterPtr cs2writer, SwitchingOutputs && switchvector):
+endpoint{endpoint}, cs2writer{cs2writer}, switchvector{std::move(switchvector)} {
+}
+
+void SwitchingOutputsHandler::operator()() {
+    for(auto const& v: switchvector) {
+        setSwitch(v.localId, v.differ);
+    }
+}
+
+void SwitchingOutputsHandler::setSwitch(std::uint8_t addr, bool r) {
+    using namespace std::chrono_literals;
+    
+    cs2writer->send(::setSwitch(::convertMMToLocId(addr), r, true));
+    std::this_thread::sleep_for(50.0ms);
+    cs2writer->send(::setSwitch(::convertMMToLocId(addr), r, false));
+    std::this_thread::sleep_for(250.0ms);
+}
---
+++ src/switchingoutputshandler.h
@@ -0,0 +1,50 @@
+/*
+ *  Project:    moba-connector
+ * 
+ *  Copyright (C) 2023 Stefan Paproth <pappi-@gmx.de>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ * 
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "moba/cs2writer.h"
+#include "moba/endpoint.h"
+#include "moba/shared.h"
+#include "item.h"
+
+class SwitchingOutputsHandler: public Item {
+public:
+
+    SwitchingOutputsHandler(EndpointPtr endpoint, CS2WriterPtr cs2writer, SwitchingOutputs && switchvector);
+    SwitchingOutputsHandler(SwitchingOutputsHandler&&) = default;
+    
+    SwitchingOutputsHandler(const SwitchingOutputsHandler&) = delete;
+    SwitchingOutputsHandler& operator=(const SwitchingOutputsHandler&) = delete;
+    
+    
+    virtual ~SwitchingOutputsHandler() noexcept = default;
+    
+    void operator()();
+    
+private:
+    EndpointPtr endpoint;
+    CS2WriterPtr cs2writer;
+    SwitchingOutputs switchvector;
+
+    void setSwitch(std::uint8_t addr, bool r);
+};
